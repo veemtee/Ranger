7ed011105fda6cbaec8f8722aafebe0366ec779b
veemtee <55981900+veemtee@users.noreply.github.com>
2021-05-17 18:36:33 +0300

    iltacommit

---
+++ Assets/Skriptit/SpawnausRekasta.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnausRekasta : MonoBehaviour
+{
+
+    public GameObject vihu1;
+    public float timer;
+    //private float adjustedTimer;
+    public int spawnAmount;
+    public int laskuri;
+    public float alkuDelay = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("spawnAmounttia", alkuDelay);
+    }
+
+    void Update()
+    {
+       
+    }
+
+    void spawnAmounttia()
+    {
+
+        InvokeRepeating("SpawnaaVihu", timer, timer);
+        
+    }
+
+ 
+    void SpawnaaVihu()
+    {
+
+        laskuri++;
+        Instantiate(vihu1.gameObject, transform.position, transform.rotation);
+        if (laskuri >= spawnAmount)
+        {
+            CancelInvoke();
+        }
+        
+
+    }
+}
+
+
---
+++ Assets/Skriptit/UralTakingDamageScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UralTakingDamageScript : MonoBehaviour
+{
+    public GameObject raato;
+
+    public int UralMaxHealth;
+    public int UralCurrentHealth;
+    public bool deadorAlive = false;
+    //public Gamemanager gameManager;
+    public ParticleSystem explosion;
+
+    private void Start()
+    {
+        UralCurrentHealth = UralMaxHealth;
+        //gameManager = GameObject.Find("GameManager").GetComponent<Gamemanager>();
+    }
+
+    private void Update()
+    {
+        if (UralCurrentHealth <= 0)
+        {
+           
+
+            if (deadorAlive == false)
+            {
+                Debug.Log("deadoralive false");
+                deadorAlive = true;
+                Instantiate(explosion.gameObject, transform.position, transform.rotation);
+                explosion.Play();
+
+                //gameManager.ScoreCount(1000);
+                Instantiate(raato.gameObject, transform.position, transform.rotation);
+                
+                Invoke("Tuhoutuminen", 0.0f);
+                
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet")
+        {
+            Debug.Log("UralOsuma");
+
+            UralCurrentHealth--;
+            
+        }
+    }
+
+    void Tuhoutuminen()
+    {
+        Destroy(gameObject);
+    }
+}
