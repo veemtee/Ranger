4d6e9a9881f109d6499be9a829aaa90850eb7194
veemtee <55981900+veemtee@users.noreply.github.com>
2021-05-29 08:11:50 +0300

    taas kaatu

---
+++ Assets/PeliAlkaaButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PeliAlkaaButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<AudioManager>().Play("Menumusic");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneManager.LoadScene("SkaleScene");
+    }
+}
